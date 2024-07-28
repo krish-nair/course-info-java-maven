2a6d904fded00374188c5de8087abad7a4dd7539
krish-nair <krish.nair101@gmail.com>
2024-07-28 14:02:15 +1200

    added httpclient code and called apli

--- src/main/java/com/pluralsight/courseinfo/cli/CourseRetriever.java
+++ src/main/java/com/pluralsight/courseinfo/cli/CourseRetriever.java
@@ -1,12 +1,13 @@
 package com.pluralsight.courseinfo.cli;
 
+import com.pluralsight.courseinfo.cli.service.CourseRetrievalService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class CourseRetriever {
     private static final Logger LOG = LoggerFactory.getLogger(CourseRetriever.class);
-    public static void main(String[] args) {
-        System.out.println("Course retriever starting...");
+    public static void main(String... args) {
+        LOG.info("Course retriever starting...");
         if (args.length == 0){
             LOG.warn("Please provide author name as first argument.");
             return;
@@ -19,6 +20,10 @@ public class CourseRetriever {
     }
 
     private static void retrieveCourses(String authorId) {
-        LOG.info("Retrieving courses for '{}' ", authorId);
+        LOG.info("Retrieving courses for '{}'", authorId);
+        CourseRetrievalService courseRetrievalService = new CourseRetrievalService();
+
+        String coursesToStore = courseRetrievalService.getCoursesFor(authorId);
+        LOG.info("Retrieved the following courses: {}", coursesToStore);
     }
 }
---
+++ src/main/java/com/pluralsight/courseinfo/cli/service/PluralsightCourse.java
@@ -0,0 +1,4 @@
+package com.pluralsight.courseinfo.cli.service;
+
+public record PluralsightCourse() {
+}
