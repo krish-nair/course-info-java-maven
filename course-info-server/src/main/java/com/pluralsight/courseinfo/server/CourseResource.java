c60ca4f40878d3595dd33a4f2d19bf3e3af53a9a
krish-nair <krish.nair101@gmail.com>
2024-07-29 17:25:33 +1200

    added course server

---
+++ course-info-server/src/main/java/com/pluralsight/courseinfo/server/CourseResource.java
@@ -0,0 +1,37 @@
+package com.pluralsight.courseinfo.server;
+
+import com.pluralsight.courseinfo.domain.Course;
+import com.pluralsight.courseinfo.domain.repository.CourseRepository;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.awt.*;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Path("/courses")
+public class CourseResource {
+    private static final Logger LOG = LoggerFactory.getLogger(CourseResource.class);
+
+    private final CourseRepository courseRepository;
+
+    public CourseResource(CourseRepository courseRepository) {
+        this.courseRepository = courseRepository;
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Course> getCourses(){
+        return courseRepository
+                .getAllCourses()
+                .stream()
+                .sorted(Comparator.comparing(Course::id))
+                .toList();
+    }
+}
